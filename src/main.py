d6421b57e92e05236dde5c6fb3e890b9c3d713f3
Frida Mjaria <frida@stitch.money>
2023-07-26 16:28:42 +0200

    Add endpoint for user authorization flow

--- src/main.py
+++ src/main.py
@@ -1,7 +1,12 @@
+import base64
+import hashlib
 import requests
+import secrets
 import uvicorn
 
 from fastapi import FastAPI
+from urllib.parse import urlencode
+
 from settings import settings
 
 
@@ -23,8 +28,52 @@ async def get_client_access_token():
     
     return response.json()
 
+@app.get("/user_auth_url")
+async def get_user_authorization_url():
+    def generate_nonce_or_state():
+        bytes = secrets.token_bytes(32)
+        hex = bytes.hex()
+        return hex
+    
+    def generate_code_verifier_and_challenge():
+        # this should be between 43 and 128
+        verifier_length = 56
+
+        code_verifier = secrets.token_urlsafe(96)[:verifier_length]
+        hashed = hashlib.sha256(code_verifier.encode('ascii')).digest()
+        encoded = base64.urlsafe_b64encode(hashed)
+        code_challenge = encoded.decode('ascii')[:-1]
+        
+        return code_verifier, code_challenge
+    
+    # 1. Obtain the auth code
+    code_verifier, code_challenge = generate_code_verifier_and_challenge()
+    
+    nonce = generate_nonce_or_state()
+    state = generate_nonce_or_state()
+    
+    query_params = {
+        "client_id": settings.STITCH_CLIENT_ID,
+        "scope": "client_paymentrequest",
+        "client_secret": settings.STITCH_CLIENT_SECRET,
+        "response_type": "code",
+        "redirect_uri": "https://localhost:8000/return",
+        "nonce": nonce,
+        "state": state,
+        "code_challenge": code_challenge,
+        "code_challenge_method": "S256",
+    }
+        
+    authorization_url = "https://secure.stitch.money/connect/authorize?" + urlencode(query_params)
+    
+    return authorization_url
+    
+
 @app.get("/return")
 async def return_url():
+    """
+    Method to handle redirect once the user has completed payment
+    """
     return 200, "Ok"
 
 
